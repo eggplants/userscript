246f52212ad8464acadc08ecc54c6524937552e0
eggplants <w10776e8w@yahoo.co.jp>
2021-11-17 06:21:38 +0900

    fixz

--- download_greasy.sh
+++ download_greasy.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
-
 readonly USERPAGEID=671442
-
 if ! command -v awk curl >/dev/null; then
+  echo "Install: awk, curl">&2
   exit 1
 fi
-
 curl -sL 'https://greasyfork.org/en/users/'"$USERPAGEID" |
   awk -F\" '/class="script-link"/{c=split($4,f,"/")
     print$4"/code/"f[c]".user.js" | "xargs -n1 curl -OL"}'
