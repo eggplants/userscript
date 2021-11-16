983cc65f79ea3d0c3fe1b0bd7b0d80af42c41b31
eggplants <w10776e8w@yahoo.co.jp>
2021-11-17 06:00:41 +0900

    fix

--- download_greasy.sh
+++ download_greasy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
+readonly USERPAGEID=671442
+
 if ! command -v awk curl >/dev/null; then
   exit 1
 fi
 
-curl -s https://greasyfork.org/en/users/671442-eggplants |
-  awk -F\" '/class="script-link"/{
-    c=split($4,f,"/");print$4"/code/"f[c]".user.js"}
-  ' | xargs -n1 curl -OL
+curl -sL 'https://greasyfork.org/en/users/'"$USERPAGEID" |
+  awk -F\" '/class="script-link"/{c=split($4,f,"/")
+    print$4"/code/"f[c]".user.js" | "xargs -n1 curl -OL"}'
